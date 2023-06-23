df487753c9e06c1cdbffe30d2877082622ba6adf
Connor White <connorwhitepdx@gmail.com>
2023-06-23 13:52:35 -0700

    adding About and reorder

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Menu from './components/Menu';
 import Landing from './components/Landing';
 import Scene from './components/Scene';
 import Loader from './components/Loader';
+import About from './components/About';
 
 function App() {
   return (
     <div className="app">
       <Loader />
       <Menu />
-      <Landing />
+      <About />
       <Scene />
+      <Landing />
     </div>
   );
 }
