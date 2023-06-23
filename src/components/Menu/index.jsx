d509c737f5d66b4a6ee09b7ab62549ad1b8f599f
Connor White <connorwhitepdx@gmail.com>
2023-06-23 14:48:37 -0700

    link hover animations

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -5,6 +5,24 @@ import './index.css';
 export default function Menu() {
     const menu = useRef();
 
+    function hoverIn(e) {
+        gsap.to(e.target, {
+            duration: 0.5,
+            letterSpacing: '3px',
+            autoRound: false,
+            ease: 'power3.inOut',
+        })
+    }
+
+    function hoverOut(e) {
+        gsap.to(e.target, {
+            duration: 0.5,
+            letterSpacing: '0px',
+            autoRound: false,
+            ease: 'power3.inOut',
+        })
+    }
+
     useEffect(() => {
         let ctx = gsap.context(() => {
             gsap.from('.navBox', {
@@ -19,8 +37,8 @@ export default function Menu() {
     }, [])
     return (
         <div ref={menu} className='menu'>
-            <div className='navBox'><a href='https://indd.adobe.com/embed/29ff1f14-a1ee-48dc-a6c2-e70bb720387a?startpage=1&allowFullscreen=true' target="_blank" rel="noopener noreferrer" className='navLink'>Our Work</a></div>
-            <div className='navBox'><a href='mailto:get@byseanbrown.com' className='navLink'>Contact</a></div>
+            <div className='navBox' onMouseEnter={hoverIn} onMouseLeave={hoverOut}><a href='https://indd.adobe.com/embed/29ff1f14-a1ee-48dc-a6c2-e70bb720387a?startpage=1&allowFullscreen=true' target="_blank" rel="noopener noreferrer" className='navLink'>Our Work</a></div>
+            <div className='navBox' onMouseEnter={hoverIn} onMouseLeave={hoverOut}><a href='mailto:get@byseanbrown.com' className='navLink'>Contact</a></div>
         </div>
     )
 }
